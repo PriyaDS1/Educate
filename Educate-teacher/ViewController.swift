388dc6d1b156f4f8d1c8d32426e4d51be1487f21
PriyaDS1 <priya.ds@enstrapp.net>
2020-03-22 10:54:05 +0530

    Initial commit

---
+++ Educate-teacher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Educate-teacher
+//
+//  Created by EnstMB4 on 22/03/20.
+//  Copyright © 2020 Enstrapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
